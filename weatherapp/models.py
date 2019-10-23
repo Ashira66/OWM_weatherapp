2e4219cf22b98f224032df64a832d13f034f7b76
Ashira66 <56925941+Ashira66@users.noreply.github.com>
2019-10-23 20:09:54 +0530

    added neccessary comments
    Removing unwanted documentations  and also added neccessary comments

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,27 +1,9 @@
 from django.db import models
-#from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
 
-#class Person(models.Model):
-#    first_name = models.CharField(max_length=20)
-#    last_name = models.CharField(max_length=20)
-#    email = models.EmailField(max_length=250)
-    #phone = PhoneNumberField()
-#   def publish(self):
-#        self.published_date = timezone.now()
-#        self.save()
-
-#    def full_name(self):
-#        return '{} {}'.format(self.first_name, self.last_name)
-
-#    def __str__(self):
-#        return self.full_name()
-
 class weatherdata(models.Model):
+    #Model for weather data
     Location = models.CharField(max_length=50)
-    #Coordinates = models.CharField(max_length=20)
-    #weather = models.CharField(max_length=30)
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
